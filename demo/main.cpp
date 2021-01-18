736cb3ffdec5a2af05701736313ad2505a6230d2
M4skEl <sivereliseu7@gmail.com>
2021-01-18 19:17:40 +0300

    do cmake list

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,19 @@
 #include <header.hpp>
+#include <boost/asio.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/thread.hpp>
+#include <chrono>
+#include <header.hpp>
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <string>
+#include <vector>
 
-int main() {
 
-}
+class talk_to_srv{
+
+};
+
+
+int main() {}
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,12 @@
 // Copyright 2020 Your Name <your_email>
 
+#include <boost/asio.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/thread.hpp>
+#include <chrono>
 #include <header.hpp>
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <string>
+#include <vector>
